4afc0008c14dcbcfe8c96b65e348b7031d29d8ea
Mosiah Adam <tech.adam.servicos@gmail.com>
2026-05-10 13:33:33 -0300

    versões acessíveis e não acessíveis

---
+++ database.js
@@ -0,0 +1,23 @@
+const products = [
+    {
+        id: 1,
+        name: 'Teclado Mecânico',
+        qty: 5,
+        desc: 'Teclado confortável com teclas grandes e boa resposta ao toque.',
+        img: 'https://cougargaming.com/global/img/products/keyboards/600k/product-image.png'
+    },
+    {
+        id: 2,
+        name: 'Mouse Sem Fio',
+        qty: 3,
+        desc: 'Mouse ergonômico e fácil de utilizar no dia a dia.',
+        img: 'https://lojamultilaser.vtexassets.com/arquivos/ids/1487173-800-auto?v=638884644826200000&width=800&height=auto&aspect=true'
+    },
+    {
+        id: 3,
+        name: 'Monitor 24 Polegadas',
+        qty: 2,
+        desc: 'Monitor com letras grandes e imagem confortável para leitura.',
+        img: 'https://netcomputadores.com.br/dbimg/produtos/p2418ht_67339_g.jpg'
+    }
+];
